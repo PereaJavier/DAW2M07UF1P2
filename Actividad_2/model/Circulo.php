5f3c08d3eec4f4e4dccafbf6fa8829e3e110baaf
PereaJavier <javvier1996@gmail.com>
2023-02-15 18:21:41 +0100

    README añadido y modificacíón Circulo añadiendo tipo

--- Actividad_2/model/Circulo.php
+++ Actividad_2/model/Circulo.php
@@ -6,9 +6,11 @@ use Actividad_2\model\Geometrico;
 require_once "Geometrico.php";
 class Circulo extends Geometrico
 {
+    private string $tipo;
     private float $radio;
     function __construct(float $radio){
         $this->radio = $radio;
+	$tipo = "concentrico";
     }
 
     public function area(): float{
